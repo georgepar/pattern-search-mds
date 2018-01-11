844cd84abd15bfa2a5919c48d604958fd134177c
thymios <etzinis@gmail.com>
2018-01-11 18:29:20 +0200

    Use external python-mnist libary for reading mnist dataset and loading in script

--- experiments/mnist/scripts/read_data_and_convert.py
+++ experiments/mnist/scripts/read_data_and_convert.py
@@ -6,12 +6,46 @@ labels available.
 \author Efthymios Tzinis"""
 
 import argparse
+from mnist import MNIST
 import os 
 
+def safe_mkdirs(path):
+    """! Makes recursively all the directory in input path
 
+    \param path (@a str) A path to create all the directories
+
+    \throws IOError If it fails to create recursive directories
+    """
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except Exception as e:
+            raise IOError(
+                ("Failed to create recursive directories: "
+                " {}".format(path)
+                )
+            )
 
 def convert_MNIST_dataset_to_numpys(MNIST_dir, converted_MNIST_dir):
-    
+    """!
+    \brief Converts the data in mnist images and labels which are in 
+    gunzip formats:
+    t10k-images-idx3-ubyte.gz
+    t10k-labels-idx1-ubyte.gz
+    train-images-idx3-ubyte.gz
+    train-labels-idx1-ubyte.gz
+    """
+    safe_mkdirs(converted_MNIST_dir)
+
+    mndata = MNIST(MNIST_dir)
+    mndata.gz=True
+    images, labels = mndata.load_training()
+
+    print type(images)
+    print type(labels)
+
+    print images[0]
+    print len(labels)
 
 def get_args():
     """! Command line parser """
@@ -30,3 +64,5 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
+    convert_MNIST_dataset_to_numpys(args.MNIST_dir, 
+                                    args.converted_MNIST_dir)
