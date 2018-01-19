d362f9ecff8d26c8072ceb33420ddbbacf3f1224
thymios <etzinis@gmail.com>
2018-01-19 16:42:32 +0200

    Run classification with 1nn for all manifold methods available except t-SNE, all variables are consistent with sacred configurationin the for loop that is responsible for testing experiments

--- experiments/mnist/scripts/run_all_configurations.py
+++ experiments/mnist/scripts/run_all_configurations.py
@@ -1,23 +1,23 @@
 from run_experiment import ex
 from read_data_and_convert import convert_MNIST_dataset_to_numpys as loader 
 from sacred.observers import MongoObserver
+from sklearn import manifold, decomposition
+import sys 
 
-target_dimensions = [20,50,75,100,150,200,300,500]
-target_dimensions = [20]
+sys.path.append('../../../')
 
-if __name__ == '__main__':
-    
-    mnistdata = loader('../dataset', '../converted_dataset')
-    train_images, train_labels, test_images, test_labels = mnistdata
 
-    n_samples, initial_dim = test_images.shape
-    n_samples = 200
-    exp_images = test_images[:n_samples]
-    exp_labels = test_labels[:n_samples]
+target_dimensions = [3,5,20,50,75,100,150,200,300,500]
 
-    for target_dim in target_dimensions:
-        # ex.observers.append(MongoObserver.create())
+target_dimensions = [5]
 
+max_turns = 1000000
+method_n_comp = 12
+exp_samples = 1000
+
+def safe_run_experiment(target_dim, exp_images,
+    exp_labels, n_samples, initial_dim, m_name, m_func):
+    try:
         ex.run( config_updates = {'target_dim' : target_dim,
                                   'data': exp_images,
                                   'initial_dim': initial_dim,
@@ -25,4 +25,72 @@ if __name__ == '__main__':
                                   'labels': exp_labels, 
                                   'n_folds': 10,
                                   'knn_algo': 'brute',
-                                  'n_neighbors': 1})   
+                                  'n_neighbors': 1,
+                                  'method_name': m_name, 
+                                  'method_func': m_func }) 
+    except Exception as e:
+        print '\n'+'='*30
+        print ('Learning Method: {} Run Time Error'
+            ''.format(m_name))
+        print '='*30 + '\n'
+    
+
+def run_all_methods_for_some_dimensions(target_dim, exp_images,
+    exp_labels, n_samples, initial_dim):
+
+    methods_l = [
+
+    ('Truncated SVD',
+    decomposition.TruncatedSVD(n_components=target_dim)),
+
+    ('LLE', 
+    manifold.LocallyLinearEmbedding(method_n_comp, target_dim,
+            eigen_solver='auto',method='standard',n_jobs=8)),
+
+    ('Isomap', manifold.Isomap(method_n_comp, target_dim)),
+
+    ('ModifiedLLE',
+    manifold.LocallyLinearEmbedding(method_n_comp,
+                                    target_dim,
+                                    eigen_solver='auto',
+                                    method='modified',n_jobs=8) ),
+
+    ('SpectralEmbedding',
+        manifold.SpectralEmbedding(n_components=target_dim,
+                                   n_neighbors=method_n_comp,
+                                   n_jobs=8)),
+
+    # ('tSNE',
+    # manifold.TSNE(n_components=target_dim, 
+    #               init='pca', random_state=0)),
+
+    ('LTSA',
+    manifold.LocallyLinearEmbedding(method_n_comp,
+                                    target_dim,
+                                    eigen_solver='auto',
+                                    method='ltsa',n_jobs=8))
+    
+    ]
+
+    for m_name, m_func in methods_l:
+        safe_run_experiment(target_dim, exp_images,
+                            exp_labels, n_samples, initial_dim, 
+                            m_name, m_func)
+         
+
+if __name__ == '__main__':
+    
+    mnistdata = loader('../dataset', '../converted_dataset')
+    train_images, train_labels, test_images, test_labels = mnistdata
+
+    n_samples, initial_dim = test_images.shape
+    n_samples = exp_samples
+    exp_images = test_images[:n_samples]
+    exp_labels = test_labels[:n_samples]
+
+    for target_dim in target_dimensions:
+
+        run_all_methods_for_some_dimensions(target_dim, exp_images,
+                                exp_labels, n_samples, initial_dim)
+
+         
--- experiments/mnist/scripts/run_experiment.py
+++ experiments/mnist/scripts/run_experiment.py
@@ -49,15 +49,18 @@ def mnist_convergence_config():
     n_folds = 10
     knn_algo = 'brute'  # {'auto', 'ball_tree', 'kd_tree', 'brute'}
     n_neighbors = 1
+    method_name = None 
+    method_func = None 
 
 @ex.automain
 def exp_main(target_dim, initial_dim, n_samples, data, labels,
-             knn_algo, n_folds, n_neighbors):
+             knn_algo, n_folds, n_neighbors,
+             method_name, method_func):
 
-    print ("Running Experiment for Target Dimensions: {}"
-        "".format(target_dim))
+    print ("Running Experiment with Smples: {}, for Target Dimensions:"" {} by using Method: {}"
+        "".format(n_samples, target_dim, method_name))
 
-    method = decomposition.TruncatedSVD(n_components=target_dim)
+    method = method_func
 
     x_embedded = method.fit_transform(data)
 
@@ -65,12 +68,32 @@ def exp_main(target_dim, initial_dim, n_samples, data, labels,
                                                 x_embedded.shape)
 
     kf = KFold(n_splits=10, shuffle=False, random_state=7)
+
+    from sklearn.metrics import recall_score
+    from sklearn.metrics import precision_score
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import f1_score
+
+    metrics_mapper = np.array
+
+    metrics_l = [('uw_f1',[]),
+                 ('w_f1',[]), 
+                 ('uw_rec',[]),
+                 ('w_rec',[]),
+                 ('uw_prec',[]),
+                 ('w_prec',[]),
+                 ('uw_acc',[]),
+                 ('w_acc',[])]
+    metric_dic = dict(metrics_l)
+
+    fold_cnt = 0
     
     for tr_ind, te_ind in kf.split(labels):
 
-        X_train = data[tr_ind]
+        X_train = x_embedded[tr_ind]
         Y_train = labels[tr_ind]
-        X_test = data[te_ind]
+        X_test = x_embedded[te_ind]
         Y_test = labels[te_ind]
 
         knn = KNeighborsClassifier(n_neighbors=n_neighbors, 
@@ -81,8 +104,37 @@ def exp_main(target_dim, initial_dim, n_samples, data, labels,
 
         est_labels = knn.predict(X_test)
 
-        print "Actual: {}".format(Y_test)
-        print "Predic: {}".format(est_labels)
+        # print "Actual: {}".format(Y_test)
+        # print "Predic: {}".format(est_labels)
+        fold_cnt += 1
+        # print "Fold: {}".format(fold_cnt)
+        uw_f1 = f1_score(Y_test, est_labels, average='macro')
+        w_f1 = f1_score(Y_test, est_labels, average='micro')
+
+        uw_rec = recall_score(Y_test, est_labels, average='macro')
+        w_rec = recall_score(Y_test, est_labels, average='micro')
+
+        uw_prec = precision_score(Y_test, est_labels, average='macro')
+        w_prec = precision_score(Y_test, est_labels, average='micro')
+        # print recall_score(Y_test, est_labels, average='weighted')
+        w_acc = accuracy_score(Y_test, est_labels)
+        cmat = confusion_matrix(Y_test, est_labels)
+        uw_acc = (cmat.diagonal()/(1.0*cmat.sum(axis=1))).mean()
+
+        metrics_l = [('uw_f1',uw_f1),
+                     ('w_f1',w_f1), 
+                     ('uw_rec',uw_rec),
+                     ('w_rec',w_rec),
+                     ('uw_prec',uw_prec),
+                     ('w_prec',w_prec),
+                     ('uw_acc',uw_acc),
+                     ('w_acc',w_acc)]
+        
+        for k,v in metrics_l:
+            metric_dic[k].append(v)
+
+    for k,v in metric_dic.items():
+        print "{}: \t {} +-({})".format(k, np.mean(v), np.std(v))
 
 
 
